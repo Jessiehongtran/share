8f11aa855c2b22164a016e0f0202630875fb8f62
Your Name <you@example.com>
2019-09-20 14:22:21 -0700

    done shareFeed component

--- share-app/src/components/shareCard.js
+++ share-app/src/components/shareCard.js
@@ -4,11 +4,11 @@ const ShareCard = props => {
     console.log('props in ShareCard', props)
 
     return (
-        <div>
-            <p>{props.share.username} is having a {props.share.item_name} to share</p>
-            <p>Pickup at: {props.share.pickup}</p>
-            <p>Available until: {props.share.deadline}</p>
-            <p>Ideally for: {props.share.target}</p>
+        <div className="share-card">
+            <h3>{props.share.username} is having a {props.share.category_name} ({props.share.item_name}) to share</h3>
+            <p className="share-text">Pickup at: {props.share.pickup}</p>
+            <p className="share-text">Available until: {props.share.deadline}</p>
+            <p className="share-text">Ideally for: {props.share.target}</p>
         </div>
     )
 }
